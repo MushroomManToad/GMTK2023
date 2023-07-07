5a7300eee782fdd46ddb66edb2a9daa959f6aae5
MushroomManToad <mushroommantoad@hotmail.com>
2023-07-07 17:09:34 -0500

    A bunch of half broken stuff

--- Scripts/Objects/LightInteractable/VEmitter.cs
+++ Scripts/Objects/LightInteractable/VEmitter.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VEmitter : MonoBehaviour
+public class VEmitter : MonoBehaviour, ILightInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject emitter;
+    bool isLit = true;
+
+    private void FixedUpdate()
     {
-        
+        if(!isLit && emitter.activeInHierarchy)
+        {
+            emitter.SetActive(false);
+        }
+        isLit = false;   
     }
 
-    // Update is called once per frame
-    void Update()
+    public void onLit()
     {
-        
+        Debug.Log("Light");
+        emitter.SetActive(true);
+        isLit = true;
     }
 }
--- Scripts/Objects/VoidLight.cs
+++ Scripts/Objects/VoidLight.cs
@@ -8,12 +8,15 @@ public class VoidLight : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Debug.Log(transform.position + " : " + radius);
         // Get all objects
         Collider2D[] overlaps = Physics2D.OverlapCircleAll(transform.position, radius);
         // Update all objects
         foreach (Collider2D collider in overlaps)
         {
+            Debug.Log(collider);
             ILightInteractable ili = collider.gameObject.GetComponent<ILightInteractable>();
+            Debug.Log(ili);
             if(ili != null)
             {
                 ili.onLit();
