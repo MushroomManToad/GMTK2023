98c65d6e261ed72e25d8728065a1e5f5fa7b3e33
MushroomManToad <mushroommantoad@hotmail.com>
2023-07-08 18:14:31 -0500

    Anglerfish
    Look at this Anglerfish
    
    Every time I do, it makes me wish...

---
+++ Scripts/Objects/Enemies/Angler.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angler : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 initialDir;
+    [SerializeField]
+    private float range;
+
+    private Vector2 facing;
+
+    private int playerScanTimer;
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private float bulletSpeed;
+
+    [SerializeField]
+    private int bulletFrequency;
+    private int bulletTime;
+
+    private void Start()
+    {
+        facing = initialDir;
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, Mathf.Atan2(facing.y, facing.x) * (180.0f / Mathf.PI));
+    }
+
+    private void FixedUpdate()
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < range)
+        {
+            facing = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+            transform.eulerAngles = new Vector3(0.0f, 0.0f, Mathf.Atan2(facing.y, facing.x) * (180.0f / Mathf.PI));
+
+            if(bulletTime > bulletFrequency)
+            {
+                bulletTime = 0;
+                shoot();
+            }
+
+            bulletTime++;
+        }
+    }
+
+    public void shoot()
+    {
+        GameObject fastBullet = Instantiate(bullet);
+        FastBullet fastBulletScript = fastBullet.GetComponent<FastBullet>();
+        Vector2 normVec = facing.normalized;
+        if (fastBulletScript != null)
+        {
+            Vector2 normScaledVec = normVec * bulletSpeed;
+            fastBulletScript.setDirection(normScaledVec);
+        }
+        
+        fastBullet.transform.position = new Vector3(transform.position.x + normVec.x, transform.position.y + normVec.y, 0.0f);
+    }
+}
